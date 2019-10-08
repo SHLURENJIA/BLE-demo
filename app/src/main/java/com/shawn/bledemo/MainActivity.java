df8cdac585540269aafed60ab23d720a139ba4c4
SHLURENJIA <SH_LURENJIA@163.com>
2019-10-08 15:42:07 +0800

    init project

---
+++ app/src/main/java/com/shawn/bledemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shawn.bledemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
